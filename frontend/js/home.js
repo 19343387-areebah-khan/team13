996603f26260fa078b1f5bc2e25b3c653534a5bc
Taran484 <19342386@brookes.ac.uk>
2026-03-14 02:43:07 +0000

    Delete frontend/js/home.js

--- frontend/js/home.js
+++
@@ -1,8 +0,0 @@
-alert("home.js is loading");
-
-const addHabitButton = document.getElementById("add-habit");
-
-addHabitButton.addEventListener("click", function() {
-    alert("Button clicked");
-});
-
